60b3018c4287ecb28d13fa27820145c55d5f752c
Vladimir Chebotarev <vladimir.chebotarev@gmail.com>
2023-03-30 22:07:09 +0300

    Correct exit code.

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open(f"{os.path.dirname(os.path.abspath(__file__))}/requirements.txt") as r
     with open(f"{os.path.dirname(os.path.abspath(__file__))}/README.md") as readme:
         setuptools.setup(
             name="rojo",
-            version="1.0.1",
+            version="1.0.2",
             description="...written in Python",  # FIXME
             long_description=readme.read(),
             long_description_content_type="text/markdown",
